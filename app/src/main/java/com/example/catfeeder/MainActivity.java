2dc65f351ef1a730ff3142784c911b743f591150
BradEstacio <bradestacio@gmail.com>
2022-03-08 16:48:25 -0600

    Have sign in button proceed to main menu

--- app/src/main/java/com/example/catfeeder/MainActivity.java
+++ app/src/main/java/com/example/catfeeder/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.catfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenuActivity();
+            }
+        });
+    }
+
+    public void openMenuActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/catfeeder/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.catfeeder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
