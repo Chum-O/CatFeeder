de0cbd5e2fbcd7d16d123c35bb8d2b77b7c0fa3c
BradEstacio <bradestacio@gmail.com>
2022-03-09 18:22:56 -0600

    Have sign out move go to main activity

--- app/src/main/java/com/example/catfeeder/MenuActivity.java
+++ app/src/main/java/com/example/catfeeder/MenuActivity.java
@@ -2,13 +2,29 @@ package com.example.catfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+    Button button_sign_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        button_sign_out = findViewById(R.id.button_sign_out);
+        button_sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
