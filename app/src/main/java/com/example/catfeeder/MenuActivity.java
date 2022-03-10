6378725232eca8690c895d162f0422277e8993bd
BradEstacio <bradestacio@gmail.com>
2022-03-09 18:55:55 -0600

    Add more empty activities that can be accessed via buttons in menu

--- app/src/main/java/com/example/catfeeder/MenuActivity.java
+++ app/src/main/java/com/example/catfeeder/MenuActivity.java
@@ -10,7 +10,7 @@ public class MenuActivity extends AppCompatActivity {
     Button button_sign_out;
     Button button_feeder_status;
     Button button_feeder_log;
-    Button button_schedule_feedings;
+    Button button_schedule_feeding;
     Button button_activate_feeder;
     Button button_activate_laser;
     Button button_view_camera;
@@ -21,6 +21,10 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         button_sign_out = findViewById(R.id.button_sign_out);
+        button_feeder_status = findViewById(R.id.button_feeder_status);
+        button_feeder_log = findViewById(R.id.button_feeder_log);
+        button_schedule_feeding = findViewById(R.id.button_schedule_feeding);
+
         button_sign_out.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +37,18 @@ public class MenuActivity extends AppCompatActivity {
                 openFeederStatusActivity();
             }
         });
+        button_feeder_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFeederLogActivity();
+            }
+        });
+        button_schedule_feeding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScheduleFeedingActivity();
+            }
+        });
     }
 
     public void openMainActivity() {
@@ -42,6 +58,16 @@ public class MenuActivity extends AppCompatActivity {
 
     public void openFeederStatusActivity() {
         Intent intent = new Intent(this, FeederStatusActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFeederLogActivity() {
+        Intent intent = new Intent(this, FeederLogActivity.class);
+        startActivity(intent);
+    }
+
+    public void openScheduleFeedingActivity() {
+        Intent intent = new Intent(this, ScheduleFeedingActivity.class);
         startActivity(intent);
     }
 }
